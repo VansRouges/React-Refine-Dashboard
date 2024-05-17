07e4281dee19222b4dcf8471541c3893498a9c17
Vance-sama <67249759+VansRouges@users.noreply.github.com>
2024-05-17 10:44:53 +0100

    App build complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,31 @@
-import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
+import { Authenticated, GitHubBanner, Refine, WelcomePage } from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
-import {
-  ErrorComponent,
-  ThemedLayoutV2,
-  ThemedSiderV2,
-  useNotificationProvider,
-} from "@refinedev/antd";
+import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import { 
-  authProvider, 
-  dataProvider, 
-  liveProvider
-} from "./providers";
+import { authProvider, dataProvider, liveProvider } from "./providers";
+import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
 
 import routerBindings, {
   CatchAllNavigate,
   DocumentTitleHandler,
-  NavigateToResource,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
 import { App as AntdApp } from "antd";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-
-import { Home, ForgotPassword, Register, Login } from "./pages"
-import Layout from "./pages/components/layout";
-
+import Layout from "./components/layout";
+import { resources } from "./config/resources";
+import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
+import List from "./pages/tasks/list";
+import EditTask from "./pages/tasks/edit";
+import CreateTask from "./pages/tasks/create";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <GitHubBanner /> */}
       <RefineKbarProvider>
           <AntdApp>
             <DevtoolsProvider>
@@ -42,46 +35,22 @@ function App() {
                 notificationProvider={useNotificationProvider}
                 routerProvider={routerBindings}
                 authProvider={authProvider}
-                resources={[
-                  {
-                    name: "blog_posts",
-                    list: "/blog-posts",
-                    create: "/blog-posts/create",
-                    edit: "/blog-posts/edit/:id",
-                    show: "/blog-posts/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                  {
-                    name: "categories",
-                    list: "/categories",
-                    create: "/categories/create",
-                    edit: "/categories/edit/:id",
-                    show: "/categories/show/:id",
-                    meta: {
-                      canDelete: true,
-                    },
-                  },
-                ]}
+                resources={resources}
                 options={{
                   syncWithLocation: true,
                   warnWhenUnsavedChanges: true,
                   useNewQueryKeys: true,
-                  projectId: "7nNoUL-eWn3kX-4OhSua",
+                  projectId: "WVyLEd-4karEq-tItoeC",
                   liveMode: "auto",
                 }}
               >
                 <Routes>
                   <Route path="/register" element={<Register />} />
                   <Route path="/login" element={<Login />} />
-                  <Route
-                    path="/forgot-password"
-                    element={<ForgotPassword />}
-                  />
+                  <Route path="/forgot-password" element={<ForgotPassword />} />
                   <Route
                     element={
-                    <Authenticated
+                    <Authenticated 
                       key="authenticated-layout"
                       fallback={<CatchAllNavigate to="/login" />}
                     >
@@ -91,6 +60,19 @@ function App() {
                     </Authenticated>
                     }>
                       <Route index element={<Home />} />
+                      <Route path="/companies" >
+                        <Route index element={<CompanyList />} />
+                        <Route path="new" element={<Create />} />
+                        <Route path="edit/:id" element={<Edit />} />
+                      </Route>
+                      <Route path="/tasks" element={
+                        <List>
+                          <Outlet />
+                        </List>
+                      }>
+                        <Route path="new" element={<CreateTask />} />
+                        <Route path="edit/:id" element={<EditTask />} />
+                      </Route>
                   </Route>
                 </Routes>
                 <RefineKbar />
